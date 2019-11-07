96e77c0b731d74f8142127f8e92f44e7e849834d
Shamsullo <shamsulloismatov@gmail.com>
2019-11-08 01:35:22 +0600

    some bug exist

--- myShell.cpp
+++ myShell.cpp
@@ -4,96 +4,102 @@
 #include <unistd.h>
 #include <string.h>
 #include <unistd.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
 
-void read_command(char cmd[], char *par[])
+using namespace std;
+
+void read_command(char *par[], bool isBackground)
 {
 	char line[1024];
-	int count = 0, i = 0, j = 0;
-	char *array[100], *pch;
+    
+    int count = 0;
+    for(;;)
+    {
+        int c =  fgetc(stdin);
+        line[count++] = (char) c;
+        if (count == '\n') break;
 
-	for(;;)
-	{
-		int c =  fgetc(stdin);
-		line[count++] = (char) c;
-		if (count == '\n') break;
-	
-	}
-	if(count == 1) return;
-	pch = strtok (line, " \n");
+    }
+    
+    if(count == 1) return;
+    char *pch = strtok (line, " \n");
+    
+    int i = 0;
+    while(pch != NULL)
+    {
+    	if(strcmp(pch, "&") == 0)
+    	{
+    		isBackground = true;
+    	}
+        else
+        {		
+        	par[i++] = pch;
+        }
+        pch = strtok(NULL, " \n");
+    }
 
-	// parsing the line into words
-	while(pch != NULL){
-		array[i++] = strdup(pch);
-		pch = strtok(NULL, " \n");
-	}
+    par[i++] = 0;
 
-	strcpy(cmd, array[0]);
-	for(int j = 0; j < i; j++){
-		par[j] = array[j];
-		par[i] =  NULL; 
-	}
 }
-
 
 void prompt()
 {
-	//clearing the screen for the first time
-	static bool first_time  = true;
-	if(first_time){
-		const char* CLEAR_SCREEN_ANSI = " \e[1;1H\e[2J";
-		write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
-		first_time  = false;
-	}
-	
-	int u  = getuid();
-	//print $ for user and # for root	
-	if(u = 0)
-	{
-		printf("myShell_# ");
-	}
-	else
-	{
-		printf("myShell_$ ");
-	}
+    //clearing the screen for the first time
+    static bool first_time  = true;
+    if(first_time){
+        const char* CLEAR_SCREEN_ANSI = " \e[1;1H\e[2J";
+        write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
+        first_time  = false;
+    }
 
-
+    int u  = getuid();
+    //print $ for user and # for root       
+    if(u = 0)
+    {
+        printf("myShell_# ");
+    }
+    else
+    {
+        printf("myShell_$ ");
+    }
 }
 
 
 int main()
 {
-	char cmd[100], command[100], *parameters[20];
 
-	//invironment variable
-	char *envp[] = {(char *) "PATH=/bin", 0};
-
-	while(1){
-		prompt();
-		read_command(command, parameters); 
+    while(1){
 		
-		if(fork() < 0)
-		{
-			printf("Fork Failed");
-		}
+        prompt();
+        
+        bool isBackground = false;
 
-		if(fork() != 0) {	//it is the parent
-			
-			wait(NULL);  //wait for child
-			//check if it is a fg job
-			//if yes
-			//wait for child to end with wait()
-		}else{
-			//bin is not a right  address
-			strcpy(cmd, "/bin/");
-			strcat(cmd, command);
-			execve(cmd, parameters, envp);
-			// to check if the ecex succeed 
-			// if no, print an error
-		}
+        char *par[20];
+        read_command(par, &isBackground);
+	    
+	    if(strcmp(par[0], "exit") == 0){break;}
+        
+        int pid = fork();
+        if(pid < 0)
+        {
+        	fprintf(stderr, "Fork Failed\n");
+        }
+        else if (pid > 0 and !isBackground)
+        {
+        	wait(NULL);
+        }
+        else if(pid > 0 and isBackground){}
+        else
+        {
+        	execvp(par[0], par);
+        	fprintf(stderr, "execvp didn't failed\n");
+        }
 
-		if(strcmp(command, "exit") == 0){break;}	
 	}
 
-	return 0;
-}
+    return 0;
 
+}
